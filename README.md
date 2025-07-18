d56208884e30a076c326e1f20d6dc87d341abf87
nturf <thestraybyte@gmail.com>
2025-07-18 22:39:22 +0530

    slides parsing

---
+++ README.md
@@ -0,0 +1,5 @@
+# present.nvim
+A Neovim plugin for creating and managing presentations
+
+This plugin was madea by [@teej](github.com/tjdevreis/present.nvim) 
+I am just recreting it here for my learning purposes.
--- lua/present.lua
+++ lua/present.lua
@@ -4,7 +4,40 @@ M.setup = function()
   --do something
 end
 
-local parse_slides
+---@class present.slides
+---@field slides string[]
 
+---@param lines string[]:
+---@return present.slides
+
+
+local parse_slides = function(lines)
+  local slides = { slides = {} }
+  local current_slides = {}
+
+  local seperator = '^#'
+
+  for _, line in ipairs(lines) do
+    print(line, 'find:', line:find(seperator), '|')
+    if line:find(seperator) then
+      if #current_slides > 0 then
+        table.insert(slides.slides, current_slides)
+      end
+      current_slides = {}
+    end
+    table.insert(current_slides, line)
+  end
+  table.insert(slides.slides, current_slides)
+
+  return slides
+end
+
+vim.print(parse_slides {
+  "# Hey There",
+  'Are you awake',
+  'Lets go out',
+  '# This is title in .md',
+  'and do some fun'
+})
 
 return M
